1e15718617cf3459c995253dd4784795d6ac7452
rixyo <saleh35-3063@diu.edu.bd>
2023-06-14 13:37:00 +0600

    fixed:payment bug

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -10,35 +10,41 @@ const page:React.FC = () => {
     const {data:user,isLoading}=useCurrentUser()
     const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string)
     const token = typeof window !== 'undefined' ? localStorage?.getItem('token') : null;
-    const  line_items = [
-        {
-            price_data: {
-                currency: 'BDT',
-                product_data: {
-                  name: user?.name,
-
-               
-                },
-                unit_amount: 250 * 100, 
-              },
-              quantity: 1
+    const submit = useCallback(async () => {
+      try {
+        const { data } = await axios.post(
+          `${process.env.NEXT_PUBLIC_API_URL}/payment/create-payment-intent`,
+          { line_items: getLineItems() },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
             },
-
-    ]
+          }
+        );
     
-    const submit=useCallback(async()=>{
-        const {data} = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/payment/create-payment-intent`, {line_items}, {
-            headers:{
-                'Content-Type':'application/json',
-                Authorization: `Bearer ${token}` ,
-            }
-        }
-      
-        )
         const stripe = await stripePromise;
-        await stripe?.redirectToCheckout({sessionId: data.id})
-
-    },[user?.name])
+        await stripe?.redirectToCheckout({ sessionId: data.id });
+      } catch (error) {
+        // Handle the error, e.g., display an error message to the user
+        console.error('Error:', error);
+      }
+    }, [token]);
+    
+    const getLineItems = () => { 
+      return [
+        {
+          price_data: {
+            currency: 'BDT',
+            product_data: {
+              name: user?.name,
+            },
+            unit_amount: 250 * 100,
+          },
+          quantity: 1,
+        },
+      ];
+    };
     if (isLoading || !user) {
         return <div className="flex items-center justify-center h-full">
         <GridLoader color="#3B82F6" />
